6fa77e80a953b91e15bdaa97ec63e95bae7816b3
Sujon Mahmud <sujonmahmud@Sujons-MacBook-Air.local>
2022-12-04 12:49:26 +0600

    edit menu

--- assets/js/main.js
+++ assets/js/main.js
@@ -2,7 +2,24 @@
 
      $(document).ready(function(){
 
-          
+       jQuery('.stellarnav').stellarNav({
+          theme: 'plain',
+          breakpoint: 768,
+          menuLabel: '',
+          sticky: false,
+          position: 'static',
+          openingSpeed: 250,
+          closingDelay: 250,
+          showArrows: true,
+          phoneBtn: '',
+          phoneLabel: 'Call Us',
+          locationBtn: '',
+          locationLabel: 'Location',
+          closeBtn: false,
+          closeLabel: 'Close',
+          mobileMode: false,
+          scrollbarFix: false
+          });
      });
 
 
